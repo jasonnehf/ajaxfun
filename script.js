abc5778947732eab10bf0883f42c38caafe807e2
Jason Nehf <jasonnehf@gmail.com>
2016-02-29 17:49:27 -0800

    more fun

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 // if you want to do something AFTER a request comes back, you need to do it in the callback function
 'use strict';
-
+var pokedex;
 $(document).ready(init);
 
 function init(event)
 {
-	$("#send").click(sendRequest);
+	$('#send').click(findPokemon);
+	$.ajax({
+		method:'GET',
+		url:"http://pokeapi.co/api/v2/pokedex/1",
+		success: function(data){
+			//console.log(data);
+			// debugger;
+			pokedex = data.pokemon_entries;
+		},
+		error: function(err) {
+			console.error("error!", err);
+		},
+	});
 };
 
 function sendRequest()
 {
 	console.log('before');
 
-	$.ajax({
-		method:'GET',
-		url:"http://pokeapi.co/api/v2/pokemon",
-		success: function(data){
-			//console.log(data);
-			// debugger;
-			var $pokemon = makeListElements(data);
-			$("#pokemon_entries").append($pokemon);
-		},
-		error: function(err) {
-			console.error("error!", err);
-		},
-	});
+	
 
 	console.log('after');
 }
@@ -41,4 +41,42 @@ function makeListElements(items) {
 	return $lis;
 
 
+}
+
+
+function findPokemon()
+{
+	var name=$('#pokemonSearch').val().toLowerCase().trim();
+	var filtered=pokedex.filter(function(pokeObj) {
+		return pokeObj.pokemon_species.name===name;
+	});
+	var pokemon= filtered[0];
+	if(pokemon)
+	{
+		$.get(pokemon.pokemon_species.url, function(data) {
+			console.log("pokemon data:", data);
+			var $card = makePokemonCard(data);
+			$('#pokemon').append($card);
+		});
+	}
+	else
+	{
+		console.log("pokemon not found");
+	}
+
+}
+
+
+function makePokemonCard(data){
+	var $card=$('#template').clone();
+	$card.find('.name').text(data.name);
+	$card.find('.id').text(data.id);
+	$card.removeAttr('id');
+	// var $card=$('<div>').addClass("card");
+	// var $name=$('<div>').addClass('name');
+	// $name.text(data.name);
+	// var $id=$('<span>').addClass('id');
+	// $id.text(data.id);
+	// $card.append($name, $id);
+	return $card;
 }
